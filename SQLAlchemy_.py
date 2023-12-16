5221ccd46113ece8b3ea3a31af2c045bce9f7f02
ktoori <152300646+ktoori@users.noreply.github.com>
2023-12-17 00:12:25 +0300

    1

--- SQLAlchemy_.py
+++ SQLAlchemy_.py
@@ -22,25 +22,3 @@ q_SQLAlchemy(Q[1], 2)
 q_SQLAlchemy(Q[2], 3)
 q_SQLAlchemy(Q[3], 4)
 
-"""
-print('querry1')
-r = conn.execute(text('''SELECT "VendorID", COUNT(*) FROM table_name GROUP BY 1'''))
-r = r.all()
-for row in r:
-    print(row)
-print('querry2')
-r = conn.execute(text('''SELECT "passenger_count", AVG("total_amount")FROM table_name GROUP BY 1'''))
-r = r.all()
-for row in r:
-    print(row)
-print('querry3')
-r = conn.execute(text('''SELECT "passenger_count", EXTRACT(year FROM "tpep_pickup_datetime"), COUNT(*) FROM table_name GROUP BY 1, 2'''))
-r = r.all()
-for row in r:
-    print(row)
-print('querry4')
-r = conn.execute(text('''SELECT "passenger_count", EXTRACT(year FROM "tpep_pickup_datetime"), ROUND("trip_distance"), COUNT(*) FROM table_name GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC'''))
-r = r.all()
-for row in r:
-    print(row)
-"""
